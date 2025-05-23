3e7fa2baeb43590e8e732b954fb17e452161c4eb
antonin.bessieres <antonin.bessieres@epita.fr>
2025-05-23 16:57:35 +0200

    we hope another

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import expressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://justtheant.github.io",
+	site: "https://justtheant.github.io/nexium-web/",
 	base: "/nexium-web/",
 	outDir: "dist",
 	publicDir: "static",
